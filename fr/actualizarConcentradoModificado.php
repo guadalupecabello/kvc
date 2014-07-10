5f96bef94276e6807521e7c90e6d6f2d61e3784a
War777 <oscarResendizUaq@gmail.com>
2014-07-10 13:18:06 -0500

    Actualizacion para Bussines Unit a Concentrado Agrupado

--- fr/actualizarConcentradoModificado.php
+++ fr/actualizarConcentradoModificado.php
@@ -14,7 +14,7 @@
 		$queries['Limpiando Resuemen Concentrado'] = "delete from fr_con_agr WHERE per = '" . $_SESSION['periodo'] . "';";
 
 		$queries['Actualizando Resuemen Concentrado'] = "insert into fr_con_agr(
-							select '" . $_SESSION['periodo'] . "', c4, c21, c20, c19, c18, c24, c22, c23, sum(c15), sum(c16), sum(c17), matGro5, matGro5Des, porEjeOm, porEjeTra, porCooOpe, porCalCen, porCooTra, porEjeCpf, porEjeRep, porDemPla, porLidCal, ejeOm, ejeTra, cooOpe, calCen, cooTra, ejeCpf, ejeRep, demPla, lidCal
+							select '" . $_SESSION['periodo'] . "', c4, c21, c20, c19, c18, c24, c22, c23, sum(c15), sum(c16), sum(c17), c12, matGro5, matGro5Des, porEjeOm, porEjeTra, porCooOpe, porCalCen, porCooTra, porEjeCpf, porEjeRep, porDemPla, porLidCal, ejeOm, ejeTra, cooOpe, calCen, cooTra, ejeCpf, ejeRep, demPla, lidCal
 						        from fr_con_mod
 						        group by c4, c21, c20, c19, c18, c24, c22, c23
 						);";
